da0dc3312f5db94eccd369d3746effa440fecc46
dorofiykolya <dorofiy.kolya@gmail.com>
2022-11-17 15:44:17 +0200

    fix identity name

--- src/Service.Identity/Repositories/UserClaims.cs
+++ src/Service.Identity/Repositories/UserClaims.cs
@@ -6,6 +6,6 @@ public class UserClaims
     public const string TypeToken = "token";
     public const string TypeName = "name";
     public const string TypeRoles = "roles";
-    public const string TypeIdentity = "identitiy";
+    public const string TypeIdentity = "identity";
     public const string TypeIdentities = "identities";
 }
